674156174196665e525bf7f1403eb48971bf8899
zueves@gmail.com <zueves@gmail.com@14c53a28-ea53-75df-6c54-14096e524f45>
2014-02-13 15:08:17 +0000

    Create tag 0.3.2

---
+++ tags/0.3.2/test/__init__.py
@@ -0,0 +1,11 @@
+﻿import unittest
+import doctests
+from trans import TestTrans
+from error import TestError
+
+suite = unittest.TestLoader().loadTestsFromModule(doctests)
+suite.addTest(unittest.makeSuite(TestTrans))
+suite.addTest(unittest.makeSuite(TestError))
+
+if __name__ == "__main__":
+    unittest.TextTestRunner(verbosity=2).run(suite)
